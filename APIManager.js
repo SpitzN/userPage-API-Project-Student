6da00af69f0222d6a101a0ca8491be50d906c1d7
SpitzN <nadav.spitzer@gmail.com>
2019-03-28 16:10:01 +0200

    initializing project

--- APIManager.js
+++ APIManager.js
@@ -5,8 +5,51 @@ class APIManager {
         this.data = {}
     }
 
-    loadData(){
+    loadData() {
         //you should make all your API requests here
         //each request should update the `data` object accordingly
+
+        $.ajax({
+            url: 'https://randomuser.me/api/?results=7',
+            dataType: 'json',
+            success: (result) => {
+                console.log(result)
+                this.data.user = {
+                    picture: result.results[0].picture.medium,
+                    first: result.results[0].name.first,
+                    last: result.results[0].name.last,
+                    city: result.results[0].location.city,
+                    state: result.results[0].location.state
+                }
+                console.log(this.data.user)
+                this.data.friends = result.results.map(f => { return { first: f.name.first, last: f.name.last } }).splice(1, 6)
+                console.log(this.data.friends)
+            }
+        });
+
+        $.get("https://pokeapi.co/api/v2/pokemon/1",
+            (result) => {
+                console.log(result)
+            }
+        );
+
+
+        
+        // $.get("https://quotes.rest/qod", 
+        //         (quotes) => {
+        //         this.data.quotes = quotes
+        //         console.log(quotes)
+        //     }
+        // );
+
+        // $.get("https://baconipsum.com/api/?type=meat-and-filler"),
+        // (bacons) => {
+        //     this.data.bacons = bacons
+
+        // }
     }
 }
+
+
+
+
--- Renderer.js
+++ Renderer.js
@@ -1,28 +1,47 @@
 // Fill in the functions for your Renderer Class
 
 class Renderer {
-    _renderUsers(users) {
+    _renderUsers(user) {
+        // $(".user-container").text("")
+        let source = $("#user-template").html()
+        let template = Handlebars.compile(source)
+        let newHTML = template(user)
+        $(".user-container").append(newHTML)
 
     }
 
-    _renderFriends(users) {
-
+    _renderFriends(friends) {
+        let source = $("#user-friends-template").html()
+        let template = Handlebars.compile(source)
+        let newHTML = template({friends})
+        $(".friends-container").append(newHTML)
     }
 
     _renderQuote(quoteInfo) {
-
+        let source = $("#quote-template").html()
+        let template = Handlebars.compile(source)
+        let newHTML = template(quoteInfo)
+        $("#quote-template").append(newHTML)
     }
 
     _renderPokemon(pokemonInfo) {
-
+        let source = $("#pokemon-template").html()
+        let template = Handlebars.compile(source)
+        let newHTML = template(pokemonInfo)
+        $(".pokemon-image").append(newHTML)
     }
 
     _renderMeat(meatText) {
-
+        let source = $("#meat-template").html()
+        let template = Handlebars.compile(source)
+        let newHTML = template(meatText)
+        $(".meat-text").append(newHTML)
     }
 
     render(data){
         //invokes all the individual _render methods
+        this._renderUsers(data.user)
+        this._renderFriends(data.friends)
     }
 }
 
